2b5ef86e460dc0a690a708478b3c8d381b27da00
rohit7t7 <rohit1471989@gmail.com>
2022-03-25 00:40:51 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,15 @@
 import { Component } from '@angular/core';
 import { VERSION } from '@angular/material';
 
-import { MatSelectSearchVersion } from 'ngx-mat-select-search';
-
+//import { MatSelectSearchVersion } from 'ngx-mat-select-search';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-
   version = VERSION;
 
-  matSelectSearchVersion = MatSelectSearchVersion;
+  // matSelectSearchVersion = MatSelectSearchVersion;
 }
